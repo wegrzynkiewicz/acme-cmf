bfdc33930f01820e65c51b81dc841e6468aa9703
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-12-10 20:37:25 +0100

    Development

--- packages/console/src/embedded/VersionCommand.unit.test.js
+++ packages/console/src/embedded/VersionCommand.unit.test.js
@@ -2,6 +2,7 @@ import sinon from 'sinon';
 import {VersionCommand} from './VersionCommand';
 
 describe('VersionCommand', () => {
+
     it('should return valid response', async () => {
         const version = '0.0.0';
         const versionCommand = new VersionCommand({
