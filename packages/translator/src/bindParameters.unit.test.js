9465b52accca70f8769a227e2001914a8dea43a3
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-12-16 22:11:24 +0100

    Development

--- packages/translator/src/bindParameters.unit.test.js
+++ packages/translator/src/bindParameters.unit.test.js
@@ -5,13 +5,17 @@ describe('bindParameters', () => {
 
     it('should bind single parameter to string', async () => {
         const actual = bindParameters('Hello {var}', {var: 'World!'});
+
         const expected = 'Hello World!';
+
         assert.strictEqual(actual, expected);
     });
 
     it('should bind many parameters to string', async () => {
         const actual = bindParameters('Hello {var}, {lang} is {var}', {lang: 'JS', var: 'World!'});
+
         const expected = 'Hello World!, JS is World!';
+
         assert.strictEqual(actual, expected);
     });
 });
